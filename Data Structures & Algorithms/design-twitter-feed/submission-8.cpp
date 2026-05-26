cfb04073b00abfcd0f92ed7cfaa1cddc9b5f825e
sanhaw154 <51456885+sanhaw154@users.noreply.github.com>
2026-05-26 18:29:57 +0800

    Add: design-twitter-feed - submission-8

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-8.cpp
@@ -0,0 +1,72 @@
+class Twitter {
+    int time = 0;
+    unordered_map<int,vector<pair<int,int>>> postRecord;
+    unordered_map<int,unordered_set<int>> follower;
+    
+public:
+    Twitter() {
+       
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        vector<pair<int,int>>& postList = postRecord[userId];
+        postList.push_back({time,tweetId});
+        time++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> res;
+        priority_queue<vector<int>> maxheap;
+        if(!follower.count(userId))
+        {
+            follower.insert({userId,{userId}});
+        }
+        unordered_set<int>& followList =follower[userId]; //該userID追隨的清單
+        for(auto followee : followList) //O(n*logn)
+        {
+	        if(postRecord.count(followee))
+	        {
+		        vector<pair<int,int>>& postList = postRecord[followee];
+            int idx = postList.size()-1;
+            maxheap.push({postList[idx].first,postList[idx].second,followee,idx-1}); //{time,tweetId,posterId,nextPostIdx}
+	        }
+        }
+        while(!maxheap.empty() && res.size() < 10 ) //O(10*logn)
+        {
+            auto& infor = maxheap.top();
+            const int& postTime = infor[0];
+            const int& tweetId = infor[1];
+            const int& posterId = infor[2];
+            const int& next_postIdx = infor[3];
+            
+            if(next_postIdx >= 0)
+            {   
+                vector<pair<int,int>>& postList = postRecord[posterId];
+                maxheap.push({postList[next_postIdx].first,postList[next_postIdx].second,posterId,next_postIdx-1});
+            }
+            res.push_back(tweetId);
+            maxheap.pop();
+        }
+        time++;
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if(!follower.count(followerId))
+        {
+            follower.insert({followerId,{followerId}});
+        }
+        follower[followerId].insert(followeeId);
+        time++;
+        return;
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(followerId != followeeId)
+        {
+            follower[followerId].erase(followeeId);
+        }
+        time++;
+        return;
+    }
+};
